52990600e63d499685b7a6ea2b7a62163de890cf
Mostafa Aly <moustafaalyyy@gmail.com>
2025-12-15 13:33:20 +0200

    refactor init state & navigate to home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Colors.black;
+
+const kTransitionDuration =  Duration(milliseconds: 500);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -19,14 +23,8 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(minutes: 1));
-
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero).animate(
-            animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHome();
   }
   @override
   void dispose() {
@@ -43,8 +41,24 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
         Image.asset(AssetsData.logo),
         const SizedBox(height: 4,),
         SlidingText(slidingAnimation:slidingAnimation),
-
       ],
     );
+  }
+
+  void initSlidingAnimation(){
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero).animate(
+            animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome(){
+    Future.delayed(const Duration(seconds: 3),(){
+      Get.to(() => const HomeView(),transition:Transition.fade, duration: kTransitionDuration);
+    });
   }
 }
