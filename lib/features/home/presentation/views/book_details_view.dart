300faa4c5b5a73066a18db9e043fce09f861b665
Mostafa Aly <moustafaalyyy@gmail.com>
2025-12-16 20:43:00 +0200

    create book_details_view.dart

---
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
+
+
+// documentation is weak
+// GetMaterialApp removes all MaterialApp
---
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
