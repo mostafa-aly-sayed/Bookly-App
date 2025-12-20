40fb30b2eae6db5e70c7bd7ef3ec321561eaed10
Mostafa Aly <moustafaalyyy@gmail.com>
2025-12-20 22:00:41 +0200

    refactor book details view body

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/recommended_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/recommendations_section.dart';
 import 'package:flutter/material.dart';
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -12,7 +9,7 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
+
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
@@ -22,54 +19,21 @@ class BookDetailsViewBody extends StatelessWidget {
             child:  Column(
               children: [
                 const CustomBookDetailsAppBar(),
-                Padding(padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-                  child: CustomBookItem(),
-                ),
-                const SizedBox(height: 43,),
-                Text('The Jungle Book', style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),),
-                const SizedBox(height: 6,),
-
-                Text(
-                  'Rudyard Kipling',
-                  style: Styles.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white.withValues(alpha: 0.7),
-                  ),
-                ),
-                const SizedBox(height: 16,),
-                const BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(height: 20,),
-                const BooksAction(),
+                BookDetailsSection(),
                 const Expanded(
                   child: SizedBox(
                     height: 20,
                   ),
                 ),
-
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20,),
-
-                const RecommendedBooksListView(),
-
+                RecommendationsSection(),
               ],
-
             ),
           ),
-        )
+        ),
       ],
     );
   }
 }
+
 
 
---
+++ lib/features/home/presentation/views/widgets/recommendations_section.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/recommended_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+class RecommendationsSection extends StatelessWidget {
+  const RecommendationsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children:[
+        Text(
+          'You can also like',
+          style: Styles.textStyle14.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 20,),
+        const RecommendedBooksListView(),
+      ],
+    );
+  }
+}
+
