0babe7f6a7cc0f3558e15316f3e6c944a0ed6042
Mostafa Aly <moustafaalyyy@gmail.com>
2026-01-02 13:53:26 +0200

    display featured books part 1

--- lib/features/home/presentation/views/widgets/books_list_view.dart
+++ lib/features/home/presentation/views/widgets/books_list_view.dart
@@ -1,22 +1,38 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BooksListView extends StatelessWidget {
   const BooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: CustomBookItem(),
-          );
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+        builder: (context, state){
+          if (state is FeaturedBooksSuccess){
+            return SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomBookItem(),
+                  );
+                },
+              ),
+            );
+          }
+          else if (state is FeaturedBooksFailure){
+            return CustomErrorWidget(errorMessage: state.errMessage);
+          }
+          else{
+            return CustomLoadingIndicator();
+          }
         },
-      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/features/home/data/repos/home_reop_implementation.dart';
 import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,10 +20,12 @@ class Bookly extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
         providers: [
-          BlocProvider(create: (context)=> FeaturedBooksCubit(
-            getIt.get<HomeRepoImplementation>(),
+          BlocProvider(
+            create: (context) => FeaturedBooksCubit(
+              getIt.get<HomeRepoImplementation>(),
+            )..fetchFeaturedBooks(),
           ),
-          ),
+
 
           BlocProvider(create: (context)=> NewestBooksCubit(
             getIt.get<HomeRepoImplementation>(),
