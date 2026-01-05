e68a557c6a88686844e91d40188b199f1b4c8618
Mostafa Aly <moustafaalyyy@gmail.com>
2026-01-05 23:35:31 +0200

    fetch similar books done

--- lib/features/home/presentation/views/widgets/recommended_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/recommended_books_list_view.dart
@@ -1,24 +1,42 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RecommendedBooksListView extends StatelessWidget {
   const RecommendedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: CustomBookItem(
-              imageURL: 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
-            ),
-          );
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+        builder: (context, state){
+          if (state is SimilarBooksSuccess)
+          {
+            return SizedBox(
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5),
+                    child: CustomBookItem(
+                      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+          else if (state is SimilarBooksFailure)
+          {
+            return CustomErrorWidget(errorMessage: state.errMessage);
+          }
+          else{
+            return const CustomLoadingIndicator();
+          }
         },
-      ),
     );
   }
 }
